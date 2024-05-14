fdc030ee30d87272bb59cddfe69c14f6de7693c1
Krivcova-VA <v.krivcova@yandex.ru>
2024-05-14 09:55:56 +0300

    add file

---
+++ partneram/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия партнерства", 
+		"/partneram/usloviya-partnerstva.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Прайс-лист", 
+		"/partneram/prays-list.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
