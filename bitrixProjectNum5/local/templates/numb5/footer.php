c81f7dade8687d797773e77fb7dc32c2935d6d0f
Krivcova-VA <v.krivcova@yandex.ru>
2024-08-31 15:46:44 +0300

    correction

--- bitrixProjectNum5/local/templates/numb5/footer.php
+++ bitrixProjectNum5/local/templates/numb5/footer.php
@@ -45,7 +45,7 @@
                         </ul>
                     </li>
                 </ul>-->
-            </div>
+
             <div class="sb_event">
                 <div class="sb_event_header"><h4>Ближайшие события</h4></div>
                 <p><a href="">29 августа 2012, Москва</a></p>
