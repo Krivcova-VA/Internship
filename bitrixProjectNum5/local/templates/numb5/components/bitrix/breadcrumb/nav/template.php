19d458f117749d07a117ac423d5c06b9e0a75ee0
Krivcova-VA <v.krivcova@yandex.ru>
2024-08-31 14:51:08 +0300

    correction

--- bitrixProjectNum5/local/templates/numb5/components/bitrix/breadcrumb/nav/template.php
+++ bitrixProjectNum5/local/templates/numb5/components/bitrix/breadcrumb/nav/template.php
@@ -11,26 +11,18 @@ $strReturn = '';
 
 
 $itemSize = count($arResult);
-?>
-<div class="bc_breadcrumbs">
-    <ul>
-<?
+
 for($index = 0; $index < $itemSize; $index++)
 {
 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
 
-//	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
-//	{
-		$strReturn .= '
-			<li><a href="'.$arResult[$index]["LINK"].'">'.$title.'</a></li>';
-//	}
-//	else
-//	{
-//		$strReturn .= $title;
-//	}
-}?>
-    </ul>
-    <div class="clearboth"></div>
-</div>
-<?php
+	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
+	{
+		$strReturn .= '<li><a href="'.$arResult[$index]["LINK"].'">'.$title.'</a></li>';
+	}
+	else
+	{
+		$strReturn .= '<li><a href="'.$arResult[$index]["LINK"].'">'.$title.'</a></li>';
+	}
+}
 return $strReturn;
