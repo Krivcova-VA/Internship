98879f69c23b6e576aa59689ab18098a94ce9a56
Krivcova-VA <v.krivcova@yandex.ru>
2024-08-31 11:16:30 +0300

    correction

--- bitrixProjectNum5/admin/.top.menu.php
+++ bitrixProjectNum5/admin/.top.menu.php
@@ -1,18 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
 $aMenuLinks = Array(
-    Array(
-        "Компания",
-        "/company/",
-        Array(),
-        Array(),
-        ""
-    ),
-    Array(
-        "Новости",
-        "/news/",
-        Array(),
-        Array(),
-        ""
-    ),
+
+);
 ?>
