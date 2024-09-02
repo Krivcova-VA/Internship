9909d00391294bc0c4bc323f946211e767a2f104
Krivcova-VA <v.krivcova@yandex.ru>
2024-09-02 22:02:58 +0300

    correction

--- bitrixProjectNum5/local/templates/numb5/components/bitrix/news/admin/bitrix/news.list/.default/template.php
+++ bitrixProjectNum5/local/templates/numb5/components/bitrix/news/admin/bitrix/news.list/.default/template.php
@@ -83,7 +83,7 @@ $this->setFrameMode(true);
         <script>
             function showMore() {
                 $(document).ready(function () {
-                    $('.'+'<?=$arItem["ID"]?>'+'more').toggle();
+                    $('.'+'<?=$arItem["ID"]?>'+'more').hide();
                     $('.'+'<?=$arItem["ID"]?>'+'qq').click(function () {
                         $(this).next().slideToggle();
                     });
