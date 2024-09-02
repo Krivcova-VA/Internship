2c13affc306d99e5701a96b79b81f8fb9a812123
Krivcova-VA <v.krivcova@yandex.ru>
2024-09-02 17:08:08 +0300

    correction

--- bitrixProjectNum5/local/templates/numb5/components/bitrix/news/admin/bitrix/news.list/.default/template.php
+++ bitrixProjectNum5/local/templates/numb5/components/bitrix/news/admin/bitrix/news.list/.default/template.php
@@ -19,7 +19,9 @@ $this->setFrameMode(true);
 <?if($arParams["DISPLAY_TOP_PAGER"]):?>
 	<?=$arResult["NAV_STRING"]?><br />
 <?endif;?>
+
 <?foreach($arResult["ITEMS"] as $arItem):?>
+    <?$n=1;?>
 	<?
 	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
 	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
@@ -79,8 +81,8 @@ $this->setFrameMode(true);
             }
         </script>
         <?echo implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 0,10)).'<span id="dots">...</span><span id="more">'.implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 10)).'</span>';?>
-        <button onclick="showMore()" id="btn">Показать больше</button>
-
+        <button onclick="showMore()" id="<?$n?>">Показать больше</button>
+        <?$n = $n+1;?>
         <?endif;?>
         <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
             <div style="clear:both"></div>
@@ -101,6 +103,7 @@ $this->setFrameMode(true);
             </small><br />
         <?endforeach;?>
         </p>
+
     <?endforeach;?>
         <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
             <br /><?=$arResult["NAV_STRING"]?>
