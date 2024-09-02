5374c30de6d74ed69a48d5945c47981c4f51fdbc
Krivcova-VA <v.krivcova@yandex.ru>
2024-09-02 22:39:36 +0300

    correction

--- bitrixProjectNum5/local/templates/numb5/components/bitrix/news/admin/bitrix/news.list/.default/template.php
+++ bitrixProjectNum5/local/templates/numb5/components/bitrix/news/admin/bitrix/news.list/.default/template.php
@@ -90,7 +90,7 @@ $this->setFrameMode(true);
                 });
             }
         </script>
-        <?echo implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 0,10)).'<span id="dots">...</span><span id= '.$arItem["ID"].'more" class="'.$arItem["ID"].'more">'.implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 8)).'</span>';?>
+        <?echo implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 0,10)).'<span id="dots">...</span><span id= '.$arItem["ID"].'more class="'.$arItem["ID"].'more">'.implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 8)).'</span>';?>
         <button onclick="showMore()" class="<?=$arItem["ID"]?>qq">Показать больше</button>
 
         <?endif;?>
