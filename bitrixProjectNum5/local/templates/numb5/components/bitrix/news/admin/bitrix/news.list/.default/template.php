aa4a507553dabb2afe15d33e434ad6b518bdb5cb
Krivcova-VA <v.krivcova@yandex.ru>
2024-09-02 18:19:43 +0300

    correction

--- bitrixProjectNum5/local/templates/numb5/components/bitrix/news/admin/bitrix/news.list/.default/template.php
+++ bitrixProjectNum5/local/templates/numb5/components/bitrix/news/admin/bitrix/news.list/.default/template.php
@@ -116,7 +116,7 @@ $this->setFrameMode(true);
         </script>
         <?$n = 1;?>
         <?echo implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 0,10)).'<span id="dots">...</span><span id="more">'.implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 10, 20)).'</span>';?>
-        <button onclick="showMore()" id="3">Показать больше</button>
+        <button onclick="showMore()" id="<?=$n?>">Показать больше</button>
         <?$n = $n+1;?>
         <?endif;?>
         <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
