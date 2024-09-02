b28b6b1d8cf2eedab95c4c9f2b2bd8fad0c00dfd
Krivcova-VA <v.krivcova@yandex.ru>
2024-09-02 19:08:25 +0300

    correction

--- bitrixProjectNum5/local/templates/numb5/components/bitrix/news/admin/bitrix/news.list/.default/template.php
+++ bitrixProjectNum5/local/templates/numb5/components/bitrix/news/admin/bitrix/news.list/.default/template.php
@@ -63,9 +63,8 @@ $this->setFrameMode(true);
 		<?endif;?>
 		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
 
-        <script>
+        <!--<script>
             function showMore() {
-                var x = 42;
                 var dots = document.getElementById("dots");
                 var moreText = document.getElementById("more");
                 var btnText = document.getElementById("btn");
@@ -81,9 +80,19 @@ $this->setFrameMode(true);
             }
         </script>
 
-        <?echo implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 0,10)).'<span id="dots">...</span><span id="more">'.implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 10, 20)).'</span>';?>
-        <button onclick="showMore()" id="btn">Показать больше</button>
 
+        <?//echo implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 0,10)).'<span id="dots">...</span><span id="more">'.implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 10, 20)).'</span>';?>
+        <button onclick="showMore()" id="btn">Показать больше</button>-->
+        <script>
+            $(document).ready(function showMore(){
+                $('.spoiler-body').hide();
+                $('.spoiler-title').click(function showMore(){
+                    $(this).next().slideToggle();
+                });
+            });
+        </script>
+        <?echo implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 0,10)).'<span id="dots">...</span><span class="spoiler-body">'.implode(' ', array_slice(explode(' ', $arItem["PREVIEW_TEXT"]), 10, 20)).'</span>';?>
+        <button onclick="showMore()" class="spoiler-title">Показать больше</button>
         <?endif;?>
         <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
             <div style="clear:both"></div>
