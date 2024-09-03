1dac700aaae9764de60cc93f831054a50fa1db6a
Krivcova-VA <v.krivcova@yandex.ru>
2024-09-03 12:25:05 +0300

    correction

--- bitrixProjectNum5/admin/index.php
+++ bitrixProjectNum5/admin/index.php
@@ -4,11 +4,10 @@ $APPLICATION->SetTitle("Административный");
 ?>
 
     <span id="push">Ваша корзина пуста</span>
+    <button id="button">Купить</button>
     <br>
     <span id="ban">Ваша корзина yt пуста</span>
-    <br>
     <button id="button">Купить</button>
-    <button id="button">Купить</button>
-    <button id="button">Купить</button>
+
     <script src="script.js"></script>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
