40effa57a9cc0f52089ea92777a6f73b2459f343
Krivcova-VA <v.krivcova@yandex.ru>
2024-09-03 12:47:53 +0300

    correction

--- bitrixProjectNum5/admin/script.js
+++ bitrixProjectNum5/admin/script.js
@@ -1,14 +1,13 @@
 let buttons = document.querySelectorAll('button[id^="button"]');
 let push = document.getElementById('push');
 let ban = document.getElementById('ban');
-for(let button of buttons){
-
-    button.addEventListener('click',function (){
-        push.innerHTML = "Добавлено в корзину";
-    })
+for(let button of buttons) {
+    if (buttons[1]) {
+        button.addEventListener('click', function () {
+            push.innerHTML = "Добавлено в корзину";
+        })
+    }
 }
-
 push.onclick = function (){
     push.innerHTML ="Ваша корзина пуста";
 }
-${buttons[0]}
