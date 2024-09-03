6d9b41fcc9ab05ea5c36d6ffa26244980d361439
Krivcova-VA <v.krivcova@yandex.ru>
2024-09-03 15:43:42 +0300

    correction

--- bitrixProjectNum5/admin/index.php
+++ bitrixProjectNum5/admin/index.php
@@ -10,5 +10,5 @@ $APPLICATION->SetTitle("Административный");
     <button id="button">Купить</button>
 
     <script src="script.js"></script>
-    <script>console.log(buttons[0]);</script>
+    <script>console.log(5676);</script>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
