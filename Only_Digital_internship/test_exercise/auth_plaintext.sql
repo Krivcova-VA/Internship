bbd483fa2dbd3e553ab71c1fc9a32932d8f3539f
Krivcova-VA <112896058+Krivcova-VA@users.noreply.github.com>
2026-09-10 16:38:08 +0300

    Разделение файлов по папкам стажировок

---
+++ Only_Digital_internship/test_exercise/auth_plaintext.sql
@@ -0,0 +1,61 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.5.29-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: auth_plaintext
+-- ------------------------------------------------------
+-- Server version	10.5.29-MariaDB-0+deb11u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `login` varchar(32) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `phone` varchar(12) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `email` varchar(254) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `session_version` int(10) unsigned NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_login` (`login`),
+  UNIQUE KEY `uq_phone` (`phone`),
+  UNIQUE KEY `uq_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Вероника','user_1','+79611886578','krivcovav174@gmail.com','123qwertyu123',1,'2026-09-09 07:14:02'),(2,'Иван','user_2','+74334343434','krivcovav1@gmail.com','123qwertyu123',1,'2026-09-09 13:50:05');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-09-10 11:55:11
---
+++ Only_Digital_internship/test_exercise/config.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+return [
+    'dsn' => 'mysql:host=127.0.0.1;dbname=auth_plaintext;charset=utf8mb4',
+    'db_user' => 'auth_plaintext',
+    'db_password' => '',
+    'captcha_site_key' => '',
+    'captcha_secret' => '',
+    'captcha_hosts' => ['testexercise.teambtest.ru'],
+    'secure_cookie' => false, 
+    'session_name' => 'auth_plaintext',
+];
